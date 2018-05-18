367a9ec85a048c8f855f27f2da2d26f9b3be738a
son-tran <tran.lamson97@gmail.com>
2018-05-18 17:38:30 +0700

    fix data

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ from preprocessing import transforming_tokens as transf
 import text_similar
 import classify
 
-model = classify.Classify()
-model.read_data("data.json")
+if __name__ == "__main__":
+    model = classify.Classify()
+    model.read_data("data.json")
 
-ts = text_similar.TextSimilar()
+    ts = text_similar.TextSimilar()
 
-# ts.pre_trained_model()
-ts.load_model()
-while True:
-    text = input("input: ")
-    if text == "q":
-        break
-    print(ts.answer(text))
+    # ts.pre_trained_model()
+    ts.load_model()
+    while True:
+        text = input("input: ")
+        if text == "q":
+            break
+        print(ts.answer(text))
--- text_similar.py
+++ text_similar.py
@@ -34,9 +34,9 @@ class TextSimilar(object):
             # self.model = models.TfidfModel.load("tmp/model.tfidf")
             self.dictionary = corpora.Dictionary.load("tmp/dictionary.dict")
             self.index =  similarities.MatrixSimilarity.load("tmp/matrix_space.index")
+            print(self.dictionary.token2id)
         except Exception:
             print("index doesn't exist")
-
 
     def create_dictionary(self):
         texts = [[token for token in transf(q)] for q in self.ques]
@@ -48,7 +48,6 @@ class TextSimilar(object):
         dictionary = corpora.Dictionary(texts)
         dictionary.save("tmp/dictionary.dict")
 
-
     def create_model(self):
         dictionary = corpora.Dictionary.load("tmp/dictionary.dict")
         corpus = [dictionary.doc2bow(transf(q)) for q in self.ques]
@@ -58,6 +57,7 @@ class TextSimilar(object):
         # model = models.LsiModel(corpus_tfidf, id2word=dictionary)
         # model.save("tmp/model.lsi")
 
+    # for a very first time run
     def pre_trained_model(self):
         self.create_dictionary()
         self.create_model()
@@ -68,6 +68,7 @@ class TextSimilar(object):
 
     def answer(self, input):
         ques_num = self.predict(input)
+        print(ques_num)
         if ques_num == 0:
             return "Xin lỗi, mình chưa hiểu ý của bạn"
         return self.ans[ques_num]
